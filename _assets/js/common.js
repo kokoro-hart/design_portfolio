c53dcf85a28c119de9c986ad76ffc5cbf7ee058d
kokoro <cocosyu.11261126@gmail.com>
2022-01-11 21:06:49 +0900

    Add: common.js

--- _assets/js/common.js
+++ _assets/js/common.js
@@ -1,9 +1,45 @@
-/**
- * @modules : node_modulesフォルダまでの絶対パスのエイリアス
- * webpack.config.jsにて定義している
- */
 
-//jQueryを使うとき
-//import $ from "@modules/jquery";
-//slickを使うとき
-// import '@modules/slick-carousel';
+//デバイス幅360px以下はリサイズして表示
+!(function () {
+  const viewport = document.querySelector('meta[name="viewport"]');
+  function switchViewport() {
+    const value =
+      window.outerWidth > 360
+        ? 'width=device-width,initial-scale=1'
+        : 'width=360';
+    if (viewport.getAttribute('content') !== value) {
+      viewport.setAttribute('content', value);
+    }
+  }
+  addEventListener('resize', switchViewport, false);
+  switchViewport();
+})();
+
+// タッチデバイスの場合:hover無効化
+const touch = 'ontouchstart' in document.documentElement || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
+if (touch) {
+  try {
+    for (let si in document.styleSheets) {
+      const styleSheet = document.styleSheets[si];
+      if (!styleSheet.rules) continue;
+
+      for (let ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+        if (!styleSheet.rules[ri].selectorText) continue;
+
+        if (styleSheet.rules[ri].selectorText.match(':hover')) {
+          styleSheet.deleteRule(ri);
+        }
+      }
+    }
+  } catch (ex) { }
+}
+
+//fv発火
+const fvTrigger = document.getElementById('js-first-view');
+function fvAnimation() {
+  fvTrigger.classList.add('is-active');
+}
+window.addEventListener('DOMContentLoaded', () => {
+  fvAnimation();
+});
+
