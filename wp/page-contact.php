fc41ed5b6b0fa3c93d040ea69f1534085bbc489b
kokoro <cocosyu.11261126@gmail.com>
2022-01-19 23:53:11 +0900

    Add: お問い合わせページを追加

---
+++ wp/page-contact.php
@@ -0,0 +1,28 @@
+
+<?php get_header(); ?>
+
+<main class="l-lower-contact">
+  <div class="l-container">
+    <div id="js-first-view" class="p-lower-first-view">
+      <h2 class="c-page-heading">
+        <span class="c-page-heading__en is-wrap">
+          <span class="is-slide-up">
+            CONTACT
+          </span>
+        </span>
+        <span class="c-page-heading__ja is-wrap">
+          <span class="is-slide-up">
+            お問い合わせ
+          </span>
+        </span>
+      </h2>
+    </div>
+    <div class="l-lower-contact__contents">
+      <div class="p-form">
+        <?php the_content(); ?>
+      </div>
+    </div>
+  </div>
+</main>
+
+<?php get_footer(); ?>
