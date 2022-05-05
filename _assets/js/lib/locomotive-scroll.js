313f21af370ebd633e0a66e828a438a8e6d01f60
kokoro <cocosyu.11261126@gmail.com>
2022-05-05 16:16:40 +0900

    Fix: locomotive.scrollをimportで読み込み

--- _assets/js/lib/locomotive-scroll.js
+++ _assets/js/lib/locomotive-scroll.js
@@ -1,4 +1,5 @@
-let scroll = new LocomotiveScroll({
+import LocomotiveScroll from '@modules/locomotive-scroll/dist/locomotive-scroll.js';
+const scroll = new LocomotiveScroll({
   el: document.querySelector('[data-scroll-container]'),
   smooth: true,
   multiplier: 1,
